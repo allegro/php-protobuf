75925fc224218a191f56335521ef7f8250a68e7f
s.perelygin <s.perelygin@gmail.com>
2016-08-17 13:46:11 -0700

    Add missing tests/Baz.php

---
+++ tests/Baz.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Auto generated from test.proto at 2016-08-16 02:35:04
+ */
+
+namespace {
+/**
+ * Baz message
+ */
+class Baz extends \ProtobufMessage
+{
+    /* Field index constants */
+    const ID = 1;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::ID => array(
+            'name' => 'id',
+            'required' => false,
+            'type' => \ProtobufMessage::PB_TYPE_INT,
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::ID] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'id' property
+     *
+     * @param integer $value Property value
+     *
+     * @return null
+     */
+    public function setId($value)
+    {
+        return $this->set(self::ID, $value);
+    }
+
+    /**
+     * Returns value of 'id' property
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->get(self::ID);
+    }
+}
+}
