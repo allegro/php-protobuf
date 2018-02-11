afe8736e69acd77004b7d979c0c8788d83668a9e
Hubert Jagodziński <hubert.jagodzinski@gmail.com>
2018-02-11 14:49:10 +0100

    Version 0.12.2

--- src/Allegro/Protobuf/Compiler/Compiler.php
+++ src/Allegro/Protobuf/Compiler/Compiler.php
@@ -6,7 +6,7 @@ use Google\Protobuf\Compiler\CodeGeneratorRequest;
 class Compiler
 {
     const MINIMUM_PROTOC_VERSION = '2.6.0';
-    const VERSION = '0.12.1';
+    const VERSION = '0.12.2';
 
     /**
      * @param string $pluginExecutable
