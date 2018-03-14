cea132515031c7a712b4444227ccf565894acda1
Hubert Jagodziński <hubert.jagodzinski@gmail.com>
2018-03-14 08:56:17 +0100

    Removed incorrectly implemented group skipping

--- src/Allegro/Protobuf/Compiler/PhpGenerator.php
+++ src/Allegro/Protobuf/Compiler/PhpGenerator.php
@@ -235,9 +235,6 @@ class PhpGenerator
                         . 'oneof is not supported (https://github.com/allegro/php-protobuf/issues/72).');
                     $ignoredOneofs[] = $oneofIndex;
                 }
-            } else if ($fieldDescriptorProto->getType()) {
-                $name = $fieldDescriptorProto->getName();
-                Logger::warn("Ignoring '${name}' field, groups are not supported.");
             } else {
                 $fieldDescriptor = new FieldDescriptor();
                 $fieldDescriptor->setName($fieldDescriptorProto->getName());
