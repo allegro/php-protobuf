ce0e4c79b4169bf3ddb28807caac3e42473957ca
Hubert Jagodziński <hubert.jagodzinski@gmail.com>
2019-01-23 08:22:04 +0100

    Version 0.9.1

--- php_protobuf.h
+++ php_protobuf.h
@@ -5,7 +5,7 @@
 #include "config.h"
 #endif
 
-#define PHP_PROTOBUF_VERSION "0.11.0"
+#define PHP_PROTOBUF_VERSION "0.9.1"
 #define PHP_PROTOBUF_EXTNAME "allegro_protobuf"
 
 extern zend_module_entry allegro_protobuf_module_entry;
--- src/Allegro/Protobuf/Compiler/Compiler.php
+++ src/Allegro/Protobuf/Compiler/Compiler.php
@@ -6,7 +6,7 @@ use Google\Protobuf\Compiler\CodeGeneratorRequest;
 class Compiler
 {
     const MINIMUM_PROTOC_VERSION = '2.6.0';
-    const VERSION = '0.11.0';
+    const VERSION = '0.9.1';
 
     /**
      * @param string $pluginExecutable
